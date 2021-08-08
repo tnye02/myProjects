b9ff60f6c056bc4154dbcbb2d24752cb3d9d4b01
tnye02 <tnye02@gmail.com>
2021-08-08 16:05:20 -0700

    Python/Database Assignment - Step 222

---
+++ Python/Database Project - Step 222/step_222.py
@@ -0,0 +1,29 @@
+
+# imports
+import sqlite3
+
+# db connection/creation variable
+conn = sqlite3.connect('txt_files.db')
+
+# supplied list of files variable
+fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+
+# creates the table if it doesn't exist
+with conn:
+    cur = conn.cursor() 
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_txtFiles( \
+                ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+                col_fileName TEXT \
+                )")
+    conn.commit()
+
+#for loop to iterate through file names
+for files in fileList:
+    if files.endswith('.txt'): '''checks the .txt file'''
+        cur.execute("INSERT INTO tbl_txtFiles(col_fileName) VALUES (?)", \
+                    (files,))
+        print(files)
+
+conn.close()
+
